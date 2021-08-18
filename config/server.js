e09a54094fa5a17ef27578580043d796e5853a34
aljubaer <bsse0812@iit.du.ac.bd>
2021-08-18 14:04:42 +0600

    setup strapi cms

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '87b84afcd44da8dff83e891889668453'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'efbd2f6f-b245-426d-8bca-47044a750341'
+};
